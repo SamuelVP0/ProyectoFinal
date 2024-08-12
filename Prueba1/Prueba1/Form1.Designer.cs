1619acc4397681c7699e95a32dbf86297f0c8450
IsmaelDP13 <ismaelde3@alumno.utesa.edu>
2024-08-11 20:17:13 -0400

    ECG
    Este es un ejemplo de un electro cardiograma

---
+++ Prueba1/Prueba1/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Prueba1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.chartECG = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chartECG)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartECG
+            // 
+            this.chartECG.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chartECG.Location = new System.Drawing.Point(12, 12);
+            this.chartECG.Name = "chartECG";
+            this.chartECG.Size = new System.Drawing.Size(760, 400);
+            this.chartECG.TabIndex = 0;
+            this.chartECG.Text = "chart1";
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(12, 418);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStartStop.TabIndex = 1;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.chartECG);
+            this.Name = "Form1";
+            this.Text = "ECG Simulation";
+            ((System.ComponentModel.ISupportInitialize)(this.chartECG)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartECG;
+        private System.Windows.Forms.Button btnStartStop;
+    }
+}
---
+++ Prueba1/Prueba1/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Prueba1
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer;
+        private Random random;
+        private int timeElapsed;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+            InitializeTimer();
+            random = new Random();
+        }
+
+        private void InitializeChart()
+        {
+            chartECG.Series.Clear();
+            chartECG.ChartAreas.Clear();
+            chartECG.ChartAreas.Add(new ChartArea("MainArea"));
+            Series series = new Series("ECG Data")
+            {
+                ChartType = SeriesChartType.Line,
+                Color = System.Drawing.Color.Red,
+                BorderWidth = 2
+            };
+            chartECG.Series.Add(series);
+        }
+
+        private void InitializeTimer()
+        {
+            timer = new Timer();
+            timer.Interval = 100; // Update every 100 ms
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            timeElapsed++;
+            double yValue = 100 * Math.Sin(timeElapsed / 10.0) + random.Next(-10, 10);
+            chartECG.Series[0].Points.AddY(yValue);
+
+            if (chartECG.Series[0].Points.Count > 100) // Keep last 100 points
+            {
+                chartECG.Series[0].Points.RemoveAt(0);
+            }
+
+            chartECG.Invalidate();
+        }
+
+        private void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (timer.Enabled)
+            {
+                timer.Stop();
+                btnStartStop.Text = "Start";
+            }
+            else
+            {
+                timer.Start();
+                btnStartStop.Text = "Stop";
+            }
+        }
+    }
+}
